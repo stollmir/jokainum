06124fd11cd7334bf915f0dcca9fa76659611512
stollmir <mirko.stoll@hs-furtwangen.de>
2019-03-24 15:45:03 +0100

    ddf

--- Aufgaben/Aufgabe0/js/main.ts
+++
@@ -1,7 +0,0 @@
-function ClickMe():void {
-                             var student=prompt ("Name ");
-
-if (student!=null) {
-                    document.getElementById("allert").innerHTML= "Hello " + student;
-}
-}
