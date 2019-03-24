e719f4772a4b27077d46782802341b9e835c87b5
stollmir <mirko.stoll@hs-furtwangen.de>
2019-03-24 13:08:39 +0100

    testing#

--- Aufgaben/Aufgabe0/main.js
+++ Aufgaben/Aufgabe0/main.js
@@ -1,4 +1,4 @@
-function klickmichan() {
+function clickme() {
     var person = prompt("Bitte füge deinen Namen ein!");
     if (person != null) {
         document.getElementById("Meldung").innerHTML = "Hey " + person + ". Was geht ab?";
