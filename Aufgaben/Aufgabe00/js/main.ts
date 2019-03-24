ca68d84286bca2a38cc2ce7734c2e95dabd669a6
stollmir <mirko.stoll@hs-furtwangen.de>
2019-03-24 13:50:08 +0100

    DFG

---
+++ Aufgaben/Aufgabe00/js/main.ts
@@ -0,0 +1,7 @@
+function klickmichan():void {
+    var person=prompt ("Bitte füge deinen Namen ein!");
+
+if (person!=null) {
+    document.getElementById("Meldung").innerHTML= "Hey " + person + ". Was geht ab?";
+}
+}
