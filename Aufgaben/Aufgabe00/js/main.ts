aefcb138c15a3dabd63ac438c750bf5797f8052e
stollmir <mirko.stoll@hs-furtwangen.de>
2019-03-24 13:40:43 +0100

    FD

--- Aufgaben/Aufgabe00/js/main.ts
+++
@@ -1,8 +0,0 @@
-function Clickme {
-    var person = prompt ("Please enter your name");
-
-if (person !=null) {
-    document.getElementById("Hello").innerHTML =
-    "Hello " + person + "! How are you today?";
-}
-}
