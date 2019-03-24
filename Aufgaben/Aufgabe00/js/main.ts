f48ba2a7027bda5b02b81d7277a62696f266d3bd
stollmir <mirko.stoll@hs-furtwangen.de>
2019-03-24 13:40:00 +0100

    BGFG

---
+++ Aufgaben/Aufgabe00/js/main.ts
@@ -0,0 +1,8 @@
+function Clickme {
+    var person = prompt ("Please enter your name");
+
+if (person !=null) {
+    document.getElementById("Hello").innerHTML =
+    "Hello " + person + "! How are you today?";
+}
+}
