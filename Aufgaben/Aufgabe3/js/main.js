9dac6a9f20b2095c1c53231fbb14b40399623268
stollmir <mirko.stoll@hs-furtwangen.de>
2019-04-18 10:59:34 +0200

    new

--- Aufgaben/Aufgabe3/js/main.js
+++ Aufgaben/Aufgabe3/js/main.js
@@ -1,61 +1,101 @@
-//Lege Array an
-let stack = ["♥7", "♥8", "♥9", "♥10", "♥J", "♥D", "♥K", "♥A", "♦7", "♦8", "♦9", "♦10", "♦J", "♦D", "♦K", "♦A", "♣7", "♣8", "♣9", "♣10", "♣J", "♣D", "♣K", "♣A", "♠7", "♠8", "♠9", "♠10", "♠J", "♠D", "♠K", "♠A",];
-//Handkarten array
-let handcards = [];
-//Neues Handkarten array
-let newhandcards = [];
-//Ablagen array
-let filing = [];
-//Eingabewert von "prmopt()" als Int
-let amountInt = parseInt(prompt("Karten"), 10);
-document.addEventListener("DOMContentLoaded", createCard);
-document.addEventListener("click", pushCard);
-document.addEventListener('click', pullCard);
-//document.addEventListener('keydown',space) {
-// if(keyCode==32){
-// pullCard();
-// }
-//else{return;}
-//}
-document.addEventListener("DOMContentLoaded", createCard);
-for (let i = 0; i < amountInt; i++) {
-    //Zuffallswahl aus "Stack-Array"
-    function randomPicking() {
-        //Generiert Zufallswert zwischen "0" und "32"
-        let y = Math.floor(Math.random() * stack.length);
-        let pick = stack.splice(y, 1);
-        handcards.push(pick[0]);
-        return pick;
-    }
-    console.log(randomPicking()[0]);
-}
-console.log("handcards: " + handcards);
-function createCard() {
+var Aufgabe3;
+(function (Aufgabe3) {
+    //Lege Array an
+    let stack = ["♥7", "♥8", "♥9", "♥10", "♥J", "♥D", "♥K", "♥A", "♦7", "♦8", "♦9", "♦10", "♦J", "♦D", "♦K", "♦A", "♣7", "♣8", "♣9", "♣10", "♣J", "♣D", "♣K", "♣A", "♠7", "♠8", "♠9", "♠10", "♠J", "♠D", "♠K", "♠A",];
+    //Handkarten array
+    let handcards = [];
+    //Ablagen array
+    let filing = [];
+    //Eingabewert von "prmopt()" als Int
+    let amountInt = parseInt(prompt("Karten"), 10);
+    //Erstelle Button
+    let div = document.createElement("div");
+    let sortButton = document.createElement("button");
+    div.id = "button";
+    div.appendChild(sortButton);
+    //Füge Body Button hinzu    
+    document.body.appendChild(div);
+    //EventListener hinzufügen
+    document.addEventListener("DOMContentLoaded", createCards);
+    window.onload = function load() {
+        document.getElementById("handcards").addEventListener("click", pushCard);
+        document.getElementById("stack").addEventListener("click", pullCard);
+        document.body.addEventListener("keydown", spacePull);
+        document.getElementById("button").addEventListener("click", sort);
+    };
     for (let i = 0; i < amountInt; i++) {
+        //Zuffallswahl aus "Stack-Array"
+        function randomPicking() {
+            //Generiert Zufallswert zwischen "0" und "32"
+            let y = Math.floor(Math.random() * stack.length);
+            let pick = stack.splice(y, 1);
+            handcards.push(pick[0]);
+            return pick;
+        }
+        console.log("randomPicking1: " + randomPicking()[0]);
+    }
+    //Erstellt Handkarten
+    function createCards() {
+        for (let i = 0; i < handcards.length; i++) {
+            let div = document.createElement("div");
+            div.style.position = "relative";
+            div.style.border = "solid pink";
+            div.style.display = "inline-block";
+            div.style.padding = "5%";
+            div.style.left = "2%";
+            div.style.width = "2%";
+            div.appendChild(document.createTextNode(handcards[i]));
+            document.getElementById("handcards").appendChild(div);
+        }
+    }
+    function pushCard(_event) {
+        //Target ermitteln 
+        //Index von "Target-Text" ermitteln
+        let target = _event.target;
+        let handcardsindex = handcards.indexOf(target.textContent);
+        console.log("handcardsindex: " + handcardsindex);
+        console.log(target);
+        //Spliced "handcard-array und pusht ins "filing-array""
+        let hold = handcards.splice(handcardsindex, 1);
+        filing.push(hold[0]);
+        console.log(hold);
+        console.log("filing" + filing);
+        //Treibt Kind ab
+        document.getElementById("handcards").removeChild(target);
+        //Fügt dem "div id= "filing-arraywert" als Text zu
+        let card = document.createTextNode(hold[0]);
+        document.getElementById("filing").innerText = card.substringData(0, 3);
+    }
+    function pullCard(_event) {
+        randomPicking();
+        //Erstelle zusätzliche Handkarte und füge Sie zum "Handcards-div" hinzu
         let div = document.createElement("div");
         div.style.position = "relative";
         div.style.border = "solid pink";
         div.style.display = "inline-block";
-        div.style.padding = "2%";
+        div.style.padding = "5%";
         div.style.left = "2%";
         div.style.width = "2%";
-        div.appendChild(document.createTextNode(handcards[i]));
+        div.appendChild(document.createTextNode(handcards[handcards.length - 1]));
         document.getElementById("handcards").appendChild(div);
     }
-}
-function pushCard(_event) {
-    let hold = handcards.splice(0, 1);
-    filing.push(hold[0]);
-    console.log("hold " + hold);
-    console.log("filing " + filing);
-    console.log("handcards " + handcards);
-    let element = document.getElementById("handcards");
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
+    function spacePull(_event) {
+        if (_event.keyCode == 32) {
+            pullCard(_event);
+        }
     }
-    createCard();
-}
-function pullCard() { }
-function sort() { }
-//console.log(pushCard());
+    function sort(_event) {
+        console.log(_event);
+        handcards.sort();
+        console.log(handcards);
+        //Entfernt"<div id="handcards"
+        document.getElementById("handcards").remove();
+        //Erstellt "<div id="handcards" neu
+        let handdiv = document.createElement("div");
+        handdiv.id = "handcards";
+        handdiv.addEventListener("click", pushCard);
+        document.body.appendChild(handdiv);
+        createCards();
+    }
+})(Aufgabe3 || (Aufgabe3 = {}));
 //# sourceMappingURL=main.js.map
--- Aufgaben/Aufgabe3/js/main.ts
+++ Aufgaben/Aufgabe3/js/main.ts
@@ -1,69 +1,118 @@
-//Lege Array an
-let stack: string[] = ["♥7", "♥8", "♥9", "♥10", "♥J", "♥D", "♥K", "♥A", "♦7", "♦8", "♦9", "♦10", "♦J", "♦D", "♦K", "♦A", "♣7", "♣8", "♣9", "♣10", "♣J", "♣D", "♣K", "♣A", "♠7", "♠8", "♠9", "♠10", "♠J", "♠D", "♠K", "♠A",];
-//Handkarten array
-let handcards: string[] = [];
-//Neues Handkarten array
-let newhandcards: string[] = [];
-//Ablagen array
-let filing: string[] = [];
-//Eingabewert von "prmopt()" als Int
-let amountInt: number = parseInt(prompt("Karten"), 10);
 
-document.addEventListener("DOMContentLoaded", createCard);
-document.addEventListener("click", pushCard);
-document.addEventListener('click', pullCard);
-//document.addEventListener('keydown',space) {
-  // if(keyCode==32){
-  // pullCard();
-// }
-//else{return;}
-//}
-document.addEventListener("DOMContentLoaded", createCard);
+namespace Aufgabe3 {
+  //Lege Array an
+  let stack: string[] = ["♥7", "♥8", "♥9", "♥10", "♥J", "♥D", "♥K", "♥A", "♦7", "♦8", "♦9", "♦10", "♦J", "♦D", "♦K", "♦A", "♣7", "♣8", "♣9", "♣10", "♣J", "♣D", "♣K", "♣A", "♠7", "♠8", "♠9", "♠10", "♠J", "♠D", "♠K", "♠A",];
+  //Handkarten array
+  let handcards: string[] = [];
+  //Ablagen array
+  let filing: string[] = [];
+  //Eingabewert von "prmopt()" als Int
+  let amountInt: number = parseInt(prompt("Karten"), 10);
+  //Erstelle Button
+  
+  let div = document.createElement("div");
+  let sortButton: HTMLButtonElement = document.createElement("button");
+  div.id = "button";
+  div.appendChild(sortButton);
 
-for (let i: number = 0; i < amountInt; i++) {
+  
+  //Füge Body Button hinzu    
+  document.body.appendChild(div);
+ 
+  //EventListener hinzufügen
+  document.addEventListener("DOMContentLoaded", createCards);
+  window.onload = function load() {
+      document.getElementById("handcards").addEventListener("click", pushCard);
+      document.getElementById("stack").addEventListener("click", pullCard);
+      document.body.addEventListener("keydown", spacePull);
+      document.getElementById("button").addEventListener("click", sort);
+      
 
-  //Zuffallswahl aus "Stack-Array"
-  function randomPicking(): string[] {
-    //Generiert Zufallswert zwischen "0" und "32"
-    let y: number = Math.floor(Math.random() * stack.length);
-    let pick: string[] = stack.splice(y, 1);
-    handcards.push(pick[0]);
-
-    return pick;
   }
-  console.log(randomPicking()[0]);
-}
-console.log("handcards: " + handcards);
 
-function createCard() {
   for (let i: number = 0; i < amountInt; i++) {
-    let div = document.createElement("div")
-    div.style.position = "relative";
-    div.style.border = "solid pink";
-    div.style.display = "inline-block";
-    div.style.padding = "2%";
-    div.style.left = "2%";
-    div.style.width = "2%";
 
-    div.appendChild(document.createTextNode(handcards[i]));
-    document.getElementById("handcards").appendChild(div);
+      //Zuffallswahl aus "Stack-Array"
+      function randomPicking(): string[] {
+          //Generiert Zufallswert zwischen "0" und "32"
+          let y: number = Math.floor(Math.random() * stack.length);
+          let pick: string[] = stack.splice(y, 1);
+          handcards.push(pick[0]);
+          return pick;
+      }
+      console.log("randomPicking1: " + randomPicking()[0]);
   }
-}
 
-function pushCard(_event: Event) {
-  let hold: string[] = handcards.splice(0, 1);
-  filing.push(hold[0]);
-  console.log("hold " + hold);
-  console.log("filing " + filing);
-  console.log("handcards " + handcards);
+  //Erstellt Handkarten
+  function createCards() {
+      for (let i: number = 0; i < handcards.length; i++) {
+          let div = document.createElement("div")
+          div.style.position = "relative";
+          div.style.border = "solid pink";
+          div.style.display = "inline-block";
+          div.style.padding = "5%";
+          div.style.left = "2%";
+          div.style.width = "2%";
 
-  let element = document.getElementById("handcards");
-  while (element.firstChild) {
-    element.removeChild(element.firstChild);
+          div.appendChild(document.createTextNode(handcards[i]));
+          document.getElementById("handcards").appendChild(div);
+      }
   }
-  createCard();
-}
 
-function pullCard() {}
-function sort(){}
-//console.log(pushCard());
+  function pushCard(_event: MouseEvent) {
+      //Target ermitteln 
+      //Index von "Target-Text" ermitteln
+      let target: HTMLElement = <HTMLElement>_event.target;
+      let handcardsindex: number = handcards.indexOf(target.textContent);
+      console.log("handcardsindex: " + handcardsindex);
+      console.log(target);
+
+      //Spliced "handcard-array und pusht ins "filing-array""
+      let hold = handcards.splice(handcardsindex, 1);
+      filing.push(hold[0]);
+      console.log(hold);
+      console.log("filing" + filing);
+
+      //Treibt Kind ab
+      document.getElementById("handcards").removeChild(target);
+
+      //Fügt dem "div id= "filing-arraywert" als Text zu
+      let card = document.createTextNode(hold[0]);
+      document.getElementById("filing").innerText = card.substringData(0, 3);
+  }
+
+  function pullCard(_event: Event) {
+      randomPicking();
+
+      //Erstelle zusätzliche Handkarte und füge Sie zum "Handcards-div" hinzu
+      let div = document.createElement("div")
+      div.style.position = "relative";
+      div.style.border = "solid pink";
+      div.style.display = "inline-block";
+      div.style.padding = "5%";
+      div.style.left = "2%";
+      div.style.width = "2%";
+
+      div.appendChild(document.createTextNode(handcards[handcards.length - 1]));
+      document.getElementById("handcards").appendChild(div);
+  }
+  function spacePull(_event: KeyboardEvent) {
+      if (_event.keyCode == 32) {
+          pullCard(_event);
+      }
+  }
+ function sort(_event: Event){
+      console.log(_event);
+      handcards.sort();
+      console.log(handcards);
+      //Entfernt"<div id="handcards"
+      document.getElementById("handcards").remove();
+
+      //Erstellt "<div id="handcards" neu
+      let handdiv: HTMLElement  = document.createElement("div");
+      handdiv.id = "handcards";
+      handdiv.addEventListener("click", pushCard);
+      document.body.appendChild(handdiv);
+      createCards();
+      }
+}
